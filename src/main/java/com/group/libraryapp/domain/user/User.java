386b9701c96b87f712f60af4ce8ab024c8206bc8
tngh4037 <tngh4037@gmail.com>
2024-06-25 05:57:40 +0900

    section4: 생애 최초 JPA 사용하기 - 유저 테이블에 대응되는 Entity Class 만들기

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,9 +1,19 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false, length = 20, name = "name") // name varchar(20) | 참고) 객체 필드명과 DB 컬럼명이 같은 경우 name="name" 은 생략할 수 있다.
     private String name;
-    private Integer age;
+
+    private Integer age; // age int // DB 테이블의 컬럼과 완전히 같기 때문에, 이런 경우는 @Column 을 생략할 수 있다.
+
+    protected User() {}
 
     public User(String name, Integer age) {
         if (name == null || name.isBlank()) {
